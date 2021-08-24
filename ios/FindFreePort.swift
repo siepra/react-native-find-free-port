4c51da127eee191d8fcd8bec27a2f1166f6f57fe
siepra <wiktor.siepra@gmail.com>
2021-08-24 10:53:07 +0200

    Provided functionality for iOS

--- ios/FindFreePort.swift
+++ ios/FindFreePort.swift
@@ -1,8 +1,55 @@
 @objc(FindFreePort)
 class FindFreePort: NSObject {
 
-    @objc(multiply:withB:withResolver:withRejecter:)
-    func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-        resolve(a*b)
+    @objc(getFirstStartingFrom:withResolver:withRejecter:)
+    func getFirstStartingFrom(port: in_port_t, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+        for i in port..<65000 {
+            let (result, _) = checkPort(port: port)
+            if result == true {
+                resolve(UInt16(i))
+                break;
+            }
+        }
     }
+
+    func checkPort(port: in_port_t) -> (Bool, descr: String) {
+
+        let socketFileDescriptor = socket(AF_INET, SOCK_STREAM, 0)
+        if socketFileDescriptor == -1 {
+            return (false, "SocketCreationFailed, \(descriptionOfLastError())")
+        }
+
+        var addr = sockaddr_in()
+        let sizeOfSockkAddr = MemoryLayout<sockaddr_in>.size
+        addr.sin_len = __uint8_t(sizeOfSockkAddr)
+        addr.sin_family = sa_family_t(AF_INET)
+        addr.sin_port = Int(OSHostByteOrder()) == OSLittleEndian ? _OSSwapInt16(port) : port
+        addr.sin_addr = in_addr(s_addr: inet_addr("0.0.0.0"))
+        addr.sin_zero = (0, 0, 0, 0, 0, 0, 0, 0)
+        var bind_addr = sockaddr()
+        memcpy(&bind_addr, &addr, Int(sizeOfSockkAddr))
+
+        if Darwin.bind(socketFileDescriptor, &bind_addr, socklen_t(sizeOfSockkAddr)) == -1 {
+            let details = descriptionOfLastError()
+            release(socket: socketFileDescriptor)
+            return (false, "\(port), BindFailed, \(details)")
+        }
+        if listen(socketFileDescriptor, SOMAXCONN ) == -1 {
+            let details = descriptionOfLastError()
+            release(socket: socketFileDescriptor)
+            return (false, "\(port), ListenFailed, \(details)")
+        }
+        release(socket: socketFileDescriptor)
+        return (true, "\(port) is free for use")
+    }
+
+    func release(socket: Int32) {
+        Darwin.shutdown(socket, SHUT_RDWR)
+        close(socket)
+    }
+
+    func descriptionOfLastError() -> String {
+        return String.init(cString: (UnsafePointer(strerror(errno))))
+    }
+    
 }
