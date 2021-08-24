efd72eca1c2842f96ca372e3726c09971a03a6dd
siepra <wiktor.siepra@gmail.com>
2021-08-24 13:14:05 +0200

    Corrected objc method interface

--- ios/FindFreePort.m
+++ ios/FindFreePort.m
@@ -2,7 +2,7 @@
 
 @interface RCT_EXTERN_MODULE(FindFreePort, NSObject)
 
-RCT_EXTERN_METHOD(multiply:(int)port
+RCT_EXTERN_METHOD(getFirstStartingFrom:(int)port
                  withResolver:(RCTPromiseResolveBlock)resolve
                  withRejecter:(RCTPromiseRejectBlock)reject)
 
